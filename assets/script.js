52004d5a8bf0e6d272c23fe769c8cbb3735af8c8
Axel Zwaans <axelzwaans@hotmail.com>
2022-06-12 18:36:25 +0000

    add code to display stats to user

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,23 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
-    guess = prompt("ready, aim, fire! (Enter a number from 0 to 6):");
-    if (guess < 0 || guess > 6)
-        alert("Please enter a valid cell number!")
-} else {
-    guess = guesses + 1;
+    guess = prompt("Ready, aim, fire! (Enter a number from 0 to 6):");
+    if (guess < 0 || guess > 6);
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || location2 || location3) {
+            hits = hits + 1;
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!")
+            }    
+        }
+    }     
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+                "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
